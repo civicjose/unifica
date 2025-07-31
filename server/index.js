b7e0a8438782605c676f4472416c4dc7d556accb
Jose Cívico <jose.civico@macrosad.com>
2025-07-31 23:17:43 +0200

    Vercel4

--- server/index.js
+++ server/index.js
@@ -34,7 +34,13 @@ app.get('/api', (req, res) => {
   res.json({ message: '¡API funcionando!' });
 });
 
+// 1. ELIMINA ESTAS LÍNEAS
+/*
 const PORT = process.env.PORT || 4000;
 app.listen(PORT, () => {
   console.log(`Servidor corriendo en el puerto ${PORT}`);
 });
+*/
+
+// 2. AÑADE ESTA LÍNEA AL FINAL
+export default app;
